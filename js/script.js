5a23081a852552d9027c5aee50194b7d940b37c7
freetimezp <kraftukrnet@ukr.net>
2023-04-13 19:50:44 +0300

    14. js-services-08-music. Preloader.

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ function scheduleTabs() {
     const tabs = document.querySelectorAll(".js-schedule-tab");
     tabs.forEach((tab) => {
         tab.addEventListener('click', () => {
-            if(tab.classList.contains("active")) {
+            if (tab.classList.contains("active")) {
                 return;
             }
             document.querySelector(".js-schedule-tab.active").classList.remove("active");
@@ -32,7 +32,7 @@ function headerMenu() {
 
     items.forEach((item) => {
         item.querySelector("a").addEventListener("click", () => {
-            if(window.innerWidth <= 991) {
+            if (window.innerWidth <= 991) {
                 menuToggle();
             }
         });
@@ -42,10 +42,15 @@ headerMenu();
 
 /* animation */
 window.addEventListener("load", () => {
-    AOS.init({
-        duration: 1200,
-        easing: 'ease-in-out-cubic',
-        once: true
+    document.querySelector(".js-preloader").classList.add("loaded");
+    document.querySelector(".js-preloader .js-bg-item").addEventListener("transitionend", () => {
+        document.querySelector(".js-preloader").style.display = "none";
+
+        AOS.init({
+            duration: 1200,
+            easing: 'ease-in-out-cubic',
+            once: true
+        });
     });
 });
 
